0b8d164ee74c94db9675014040bdc065c25e1cb1
Deniscley Marfran <deniscley.marfran@squadra.com.br>
2023-06-09 12:55:10 -0300

    feat: Serilog configuration.

--- Nutrition.And.Exercise.API/src/Nutrition.And.Exercise.Api/Configuration/FluentValidationConfig.cs
+++ Nutrition.And.Exercise.API/src/Nutrition.And.Exercise.Api/Configuration/FluentValidationConfig.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation.AspNetCore;
+using System.Globalization;
 using System.Text.Json.Serialization;
 using static Nutrition.And.Exercise.Borders.Commands.RegisterClientCommand;
 
@@ -8,6 +9,13 @@ namespace Nutrition.And.Exercise.Api.Configuration
     {
         public static void AddFluentValidationConfiguration(this IServiceCollection services)
         {
+            services.AddControllers()
+                .AddFluentValidation(x =>
+                {
+                    x.RegisterValidatorsFromAssemblyContaining<Startup>();
+                    x.ValidatorOptions.LanguageManager.Culture = new CultureInfo("pt-Br");
+                });
+
             //services.AddFluentValidationRulesToSwagger();
         }
     }
