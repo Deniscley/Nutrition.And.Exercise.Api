656750c9cbeeaceeff710e45712267c858844d42
Deniscley Marfran <deniscley.marfran@squadra.com.br>
2023-06-10 23:00:01 -0300

    feat: useCase removal.

--- Nutrition.And.Exercise.API/src/Nutrition.And.Exercise.Api/Configuration/DependencyInjectionConfig.cs
+++ Nutrition.And.Exercise.API/src/Nutrition.And.Exercise.Api/Configuration/DependencyInjectionConfig.cs
@@ -3,12 +3,10 @@ using Nutrition.And.Exercise.Borders.Commands;
 using Nutrition.And.Exercise.Borders.Events;
 using Nutrition.And.Exercise.Borders.Interfaces.Repositories.PersistenceRepositories;
 using Nutrition.And.Exercise.Borders.Interfaces.Repositories.QueryRepositories;
-using Nutrition.And.Exercise.Borders.Interfaces.UseCases;
 using Nutrition.And.Exercise.Borders.Mediator;
 using Nutrition.And.Exercise.Data.Context;
 using Nutrition.And.Exercise.Data.Repositories.PersistenceRepositories;
 using Nutrition.And.Exercise.Data.Repositories.QueryRepositories;
-using Nutrition.And.Exercise.UseCases;
 using ValidationResult = FluentValidation.Results.ValidationResult;
 
 namespace Nutrition.And.Exercise.Api.Configuration
@@ -20,9 +18,6 @@ namespace Nutrition.And.Exercise.Api.Configuration
             //// Repository
             services.AddScoped<IClientRepository, ClientRepository>();
             services.AddScoped<IClientQueryRepository, ClientQueryRepository>();
-
-            //// UseCase
-            services.AddScoped<IClientUseCase, ClientUseCase>();
 
             //// MediatorHandler
             services.AddScoped<IMediatorHandler, MediatorHandler>();
